dcf374f69989d0c123a9d68af2710bf2d5356fc8
mat-ben <matous.benedikt@gmail.com>
2026-03-27 23:45:01 +0100

    initialize project structure

---
+++ src/main/java/com/matous/chat_engine/ChatEngineApplication.java
@@ -0,0 +1,13 @@
+package com.matous.chat_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatEngineApplication.class, args);
+	}
+
+}
